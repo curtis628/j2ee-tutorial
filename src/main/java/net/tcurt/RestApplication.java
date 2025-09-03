6935535812710e4f86c8291a945c51f38966450b
Tyler Curtis <tjcurt@gmail.com>
2025-09-03 16:42:21 -0700

    Initial commit (bare-bones jax-rs example)
    ```
    $> mvn clean package && cp target/jboss-tutorial.war $JBOSS_HOME/standalone/deployments/
    $> curl localhost:8080/jboss-tutorial/api/hello
    Hello from JAX-RS on WildFly!
    ```

---
+++ src/main/java/net/tcurt/RestApplication.java
@@ -0,0 +1,9 @@
+package net.tcurt;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class RestApplication extends Application {
+    // no code needed — this activates JAX-RS in WildFly
+}
