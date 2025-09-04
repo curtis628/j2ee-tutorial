accbe2aa79d135f73dbb7757b39568216143102c
Tyler Curtis <tjcurt@gmail.com>
2025-09-04 12:45:41 -0700

    Add very basic jax-ws XML/SOAP endpoint: /soap/hello
    ```
    $> echo '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
                                                xmlns:ser="http://tcurt.net/">
                                 <soapenv:Header/>
                                 <soapenv:Body>
                                    <ser:sayHello>
                                       <name>Tyler</name>
                                    </ser:sayHello>
                                 </soapenv:Body>
                              </soapenv:Envelope>' > request.xml
    
    $> curl -X POST http://localhost:8080/jboss-tutorial/soap/hello \
                                    -H "Content-Type: text/xml;charset=UTF-8" \
                                    -d @request.xml | xmllint --format -
    <?xml version="1.0"?>
    <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
      <soap:Body>
        <ns2:sayHelloResponse xmlns:ns2="http://tcurt.net/">
          <return>Hello, Tyler from SOAP!</return>
        </ns2:sayHelloResponse>
      </soap:Body>
    </soap:Envelope>
    ```

---
+++ src/main/java/net/tcurt/HelloService.java
@@ -0,0 +1,15 @@
+package net.tcurt;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
+@WebService
+public class HelloService {
+
+    @WebMethod
+    public String sayHello(@WebParam(name = "name") String name) {
+        return "Hello, " + name + " from SOAP!";
+    }
+}
+
